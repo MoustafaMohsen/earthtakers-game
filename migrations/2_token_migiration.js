f43cf730de079eee316a299bff9aa30a8838ea8c
Moustafa Mohsen <moustafamohsen1@gmail.com>
2021-11-14 11:01:15 +0200

    generate pet and calculate lifespan based on ability and strenght

--- migrations/2_token_migiration.js
+++ migrations/2_token_migiration.js
@@ -1,9 +1,18 @@
 const Token = artifacts.require("Token");
-
+const starting_count = 3;
 module.exports = async function(deployer) {
     await deployer.deploy(Token, "NFT Game", "NFTG");
     let tokenInstance = await Token.deployed();
-    await tokenInstance.mint(100, 200, 100000); // token id 0
-    let pet = await tokenInstance.getTokenDetails(0)
-    console.log(pet);
+    var generatePet = async ()=>{
+        let baseLifespan = 1000;
+        let ability = Math.floor(Math.random()*255)
+        let strenght = Math.floor(Math.random()*255)
+        let lifespan = Math.floor(baseLifespan-baseLifespan*((ability+strenght)/2)/255)
+        await tokenInstance.mint(ability, strenght, lifespan); // token id 0
+        console.log("Generated pet",ability,strenght,lifespan);
+
+    }
+    for (let i = 0; i < starting_count; i++) {
+        generatePet();
+    }
 };
